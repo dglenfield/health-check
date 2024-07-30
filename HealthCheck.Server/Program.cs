92abe0943f2c4c949230c76e65b9e93e1cf30e9e
Danny Glenfield <dglenfield@gmail.com>
2024-07-30 12:32:17 -0400

    Add ICMPHealthCheck to the service

--- HealthCheck.Server/Program.cs
+++ HealthCheck.Server/Program.cs
@@ -1,8 +1,15 @@
+global using HealthCheck.Server;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHealthChecks()
+    .AddCheck("ICMP_01", new ICMPHealthCheck("www.amazon.com", 100))
+    .AddCheck("ICMP_02", new ICMPHealthCheck("www.google.com", 100))
+    .AddCheck("ICMP_03", new ICMPHealthCheck($"www.{Guid.NewGuid():N}.com", 100));
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -22,6 +29,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseHealthChecks(new PathString("/api/health"), new CustomHealthCheckOptions());
 
 app.MapControllers();
 
