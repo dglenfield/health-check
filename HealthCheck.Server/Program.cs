8a6043ebc4f9507ade442bd68603030170804e30
Danny Glenfield <dglenfield@gmail.com>
2024-08-14 09:55:51 -0400

    Add CORS to the Web API

--- HealthCheck.Server/Program.cs
+++ HealthCheck.Server/Program.cs
@@ -1,4 +1,5 @@
 global using HealthCheck.Server;
+using Microsoft.AspNetCore.Cors;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AngularPolicy", cfg =>
+    {
+        cfg.AllowAnyHeader();
+        cfg.AllowAnyMethod();
+        cfg.WithOrigins(builder.Configuration["AllowedCORS"]);
+    });
+});
 
 var app = builder.Build();
 
@@ -29,6 +40,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("AngularPolicy");
 
 app.UseHealthChecks(new PathString("/api/health"), new CustomHealthCheckOptions());
 
